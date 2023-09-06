c61c687eaf1e75e7ef18b505c5a2a595571a54a8
Benson Kiai <benson.kiai86@gmail.com>
2023-09-06 23:47:18 +0300

    Add input to filter by score

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../styles/Homepage.css';
@@ -7,18 +7,44 @@ import '../styles/Homepage.css';
 const HomePage = () => {
   const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
+  // State variables for filtering
+  const [scoreFilter, setScoreFilter] = useState(''); // Score filter value
+  const [filteredCategories, setFilteredCategories] = useState([]); // Filtered categories
 
   useEffect(() => {
     fetch('https://api.jikan.moe/v4/anime?q=&sfw')
       .then((res) => res.json())
       .then((data) => {
         dispatch({ type: 'SET_CATEGORIES', payload: data.data });
+        setFilteredCategories(data.data); // Initially, set filtered categories to all categories
       });
   }, [dispatch]);
 
+  // Function to handle Score filter change
+  const handleScoreFilterChange = (e) => {
+    const newScoreFilter = e.target.value;
+    setScoreFilter(newScoreFilter);
+
+    // Apply filtering based on the Score filter
+    const filtered = categories.filter((category) => category.score >= parseFloat(newScoreFilter));
+
+    setFilteredCategories(filtered);
+  };
+
   return (
     <div className="grid-container">
-      {categories.map((category) => (
+      <div className="filter">
+        <input
+          type="number"
+          className="score"
+          min="6"
+          max="8"
+          placeholder="Filter by movie rating (6-8)"
+          value={scoreFilter}
+          onChange={handleScoreFilterChange}
+        />
+      </div>
+      {filteredCategories.map((category) => (
         <Link
           to={`/details/${category.mal_id}`}
           key={category.mal_id}
